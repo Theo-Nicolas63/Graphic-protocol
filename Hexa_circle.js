7bfd86f57973e55ee66b2999d57149d684ba29dc
Theo-Nicolas63 <theo.nicolas63@gmail.com>
2023-05-12 22:54:39 +0200

    [DEV][FEAT] Ajout de détection d'erreur avec le code CRC

--- Hexa_circle.js
+++ Hexa_circle.js
@@ -3,9 +3,12 @@ let btn = document.getElementById("myBtn");
 let mySpan = document.getElementById("mySpan");
 
 //INDEX de positionnement des caractères dans le QR code
-let index = [1, 14, 2, 15, 3, 16, 4, 17, 5, 18, 6, 19, 7, 20, 8, 21, 9, 22, 10, 23, 11, 24, 12, 25, 13, 26];
+let index = [1, 14, 2, 15, 3, 16, 4, 17, 5, 18, 6, 19, 7, 20, 8, 21, 9, 22, 10, 23, 11, 24, 12, 25, 13, 26, 27, 28, 29, 30];
 
-//Tableau de caractères du message
+//string hexa du message
+let messageHex = "";
+
+//tableau de caractère hexa
 let code = [];
 
 //Tableau de code hexa de tous les caractères du message
@@ -14,21 +17,23 @@ let codeTab = [];
 //DATASET qui contient les données pour le graphique
 let mondataset = [];
 
-//Donneés pour rendondance 
+//DATASET des Donneés pour rendondance 
 let dataSetRedondance = [];
 
-console.log(toNumber('a'));
-
+//FONCTION PRINCIPALE
+// isSimple = 0 si encodage complet et = 1 si encodage sans redondance ni detection d'erreur
+// Pour comprendre vous pouvez lancer avec le bouton "Encodage simple" avec un message d'un lettre pour afficher le coage d'uniquement cette lettre
 function encoder(isSimple) {
   let x;
   let lenMessage;
-  let parite = 0;
   let i = 0;
   code = [];
   codeTab = [];
   mondataset = [];
   message = text.value;
   lenMessage = lengthMessage(message);
+
+  //LIMITE DE LONGUEUR DE MESSAGE
   if(lenMessage > 26 || lenMessage == 0){
     mySpan.innerHTML = "Votre message n'est pas valide";
     return;
@@ -36,46 +41,56 @@ function encoder(isSimple) {
   else{
     mySpan.innerHTML = "";
   }
-  code = message.split(""); // Separation de tous les caractères du message dans un tableau
-  code = indexage(code, isSimple);  
-  code.forEach(e => {
-    codeTab.push(AsciitoHexa(e));
-  });
-  console.log(codeTab);
 
-  codeTab.forEach(e => {
+  //SI CODAGE AVEC DETECTION ERREUR
+  if(isSimple == 0){
+    messageHex = stringToHex(message);
+    messageHex = addCRC(messageHex);
+    codeTab = messageHex.split("");
+  }
+  //SI CODAGE SANS DETECTION ERREUR
+  else {
+    code = message.split(""); // Separation de tous les caractères du message dans un tableau
+    code.forEach(e => {
+      codeTab.push(AsciitoHexa(e));
+    });
+  }
+
+  codeTab = indexage(codeTab, isSimple); // appel de la fonction indexage qui mixe l'odre d'apparition
+
+  codeTab.forEach(e => { // Pour chaque caractère ASCII présent dans codeTab
     x = e.split("");
     x.forEach(y => {
-      //if(pariteCondition(lenMessage, parite, x.length, i)){
-        addDataset(dataBuilder(y, 0), 1); // Construction du dataset
 
-        if(isSimple == 0)
+
+        addDataset(dataBuilder(y, 0), 1); // Construction du dataset et ajout à la liste des dataset
+
+        if(isSimple == 0 && i<27) // Si codage avec redondance et i < 27 car pas de redondance sur le CRC
           addDataset(dataBuilder(y, 1), 2); // Construction du dataset pour la redondance
-        parite++;
-     // }
-      //else {
-       // addDataset(dataBuilder(y), 0);
-      //}
+
       i++;
     });
   });
-  console.log(mondataset);
-  afficher();
+
+  afficher(); // APPEL DE LA FONCTION AFFICHER
 }
 
+// CONVERSION D'UN CARACTERE EN HEXA
 function AsciitoHexa(c) {
-  return c.charCodeAt(0).toString(16);
+  return c.charCodeAt(0).toString(16).toUpperCase();
 }
 
-function isPair(n){
-  if(n%2 == 0){
-    return true;
+//Fonction qui converti un string en sa représentation hexadecimal
+function stringToHex(str) {
+  let hex = '';
+  for (let i = 0; i < str.length; i++) {
+    hex += str.charCodeAt(i).toString(16).padStart(2, '0');
   }
-  else{
-    return false;
-  }
+  return hex.toUpperCase();
 }
 
+//FONCTION qui construit un tableau de taille 2 tel que :
+// [x, y] x et y étant les pourcentages soit x = 100 et y = 0 pour un cercle qui fera le tour complet
 function dataBuilder(hexa, x){
   if(isLetter(hexa)){ // Verification que l'hexa est une lettre, si oui alors conversion 
     console.log("C'est une lettre");
@@ -95,6 +110,7 @@ function dataBuilder(hexa, x){
   return [h2, h1]; // Retourne l'inverse pour la redondance
 }
 
+//FONCTION qui construit un dataset. Le dataset est le format a créé pour pouvoir afficher correctement
 function addDataset(data1, x){
   
   let newDataset = {
@@ -115,7 +131,7 @@ function addDataset(data1, x){
     newDataset.backgroundColor[0] = 'rgba(255, 255, 255)';
     newDataset.backgroundColor[1] = 'rgba(0, O, 0)';
     newDataset.borderColor[0] = 'rgba(0, 0, 0)';
-    newDataset.borderColor[1] = 'rgba(0, 0, 0)';
+    newDataset.borderColor[1] = 'rgba(255, 255, 255)';
     dataSetRedondance.push(newDataset);
   }
   else {
@@ -123,44 +139,47 @@ function addDataset(data1, x){
   }
 }
 
+//Fonction qui teste  si c est une lettre ou non 
 function isLetter(c) {
   return c.toLowerCase() != c.toUpperCase();
 }
 
+//Fonction qui traduit les lettres hexadecimal en leurs pourcentage de tour de cercle correspondant
 function toNumber(c){
-  let y = 100/17;
-  let x = 50 + y; // Valeur de 9  
+  let y = 100/17; // 17 car 17 zones égale dans le cercle
+  let x = 50 + y; // Valeur de la zone de 9 dans le cercle soit 50% puis pour chaque lettre on ajoutera y qui est la taille de chaque partie
+                  // A étant la première zone apres 9 etc  
   switch(c){
-    case 'a':
+    case 'A':
       return x + y;
-    case 'b':
+    case 'B':
       return x + 2*y;
-    case 'c':
+    case 'C':
       return x + 3*y;
-    case 'd':
+    case 'D':
       return x + 4*y;
-    case 'e':
+    case 'E':
       return x + 5*y;
-    case 'f':
+    case 'F':
       return x + 6*y;  
   }
 
-  console.log("Erreur : c n'est pas une lettre valide");
+  console.log("Erreur : c n'est pas une lettre valide"); // Affiche un message d'erreur dans la console du navigateur
 }
 
+//Fonction qui affiche
 function afficher(){
-  console.log(mondataset);
 
   dataSetRedondance.reverse();
   dataSetRedondance.forEach(e => {
     mondataset.unshift(e);
   });
 
-  console.log(mondataset);
+  //Affichage du graphique
 
   const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
+  new Chart(ctx, { //Création du graphique
     type: 'pie',
     data: {
       labels: ['Data', 'No Data'],
@@ -183,17 +202,19 @@ function afficher(){
 
 }
 
+//fonction qui retourne la longueur du string passé en paramètre
 function lengthMessage(message){
   message = message.split("");
   return message.length;
 }
 
-function indexage(code, x){
+//Fonction qui mix l'odre d'apparition des caractères dans le tableau
+function indexage(code, isSimple) {
   let codeRedondance = [];
-  if(x == 0){
-    codeRedondance = [26]; // Tableau de 26 cases
+  if(isSimple == 0){
+    codeRedondance = [30]; // Tableau de 30 cases
     
-    for(let i = 0; i < 26; i++){ // Initialisation du tableau
+    for(let i = 0; i < 30; i++){ // Initialisation du tableau
       codeRedondance[i] = " ";
     }
 
@@ -214,15 +235,83 @@ function indexage(code, x){
   return codeRedondance;
 }
 
-function pariteCondition(lenMessage, parite, x, i){
-  /*if(isPair(lenMessage) && !isPair(parite) && x - i > 1)
-    return true;
+//Fonction qui calcul et ajoute à un message hexadécimal son CRC correspondant
+function addCRC(hexMessage) {
+  // Convertir le message hexadécimal en binaire
+  let binaryMessage = "";
+  for (let i = 0; i < hexMessage.length; i++) {
+    binaryMessage += parseInt(hexMessage[i], 16).toString(2).padStart(4, "0");
+  }
 
-  if(!isPair(lenMessage) && isPair(parite) && x - i > 1)
-    return true;
-  */
-  return false;  
+  // Polynôme générateur utilisé pour le calcul du CRC (CRC-16-CCITT)
+  const polynomial = "11000000000000101";
+
+  // Initialisation du registre CRC à 0xFFFF
+  let crcRegister = parseInt("FFFF", 16);
+
+  // Calcul du CRC
+  for (let i = 0; i < binaryMessage.length; i++) {
+    const xorBit = (crcRegister & parseInt("8000", 16)) !== 0;
+    crcRegister <<= 1;
+    if (binaryMessage[i] === "1") {
+      crcRegister |= 1;
+    }
+    if (xorBit) {
+      crcRegister ^= parseInt(polynomial, 2);
+    }
+  }
+
+  // Ajout du CRC calculé à la fin du message binaire
+  const crc = crcRegister.toString(2).padStart(16, "0");
+  binaryMessage += crc;
+
+  // Conversion du message binaire en hexadécimal
+  let hexCRCMessage = "";
+  for (let i = 0; i < binaryMessage.length; i += 4) {
+    hexCRCMessage += parseInt(binaryMessage.substr(i, 4), 2).toString(16).toUpperCase();
+  }
+
+  return hexCRCMessage;
 }
+
+
+
+// Fonction qui vérifie que le CRC du message est valide et donc qu'il n'y pas d'erreur
+// Fonction non utilisé car décodage non demandé
+
+function checkCRC(hexCRCMessage) {
+  // Convertir le message hexadécimal en binaire
+  let binaryMessage = "";
+  for (let i = 0; i < hexCRCMessage.length; i++) {
+    binaryMessage += parseInt(hexCRCMessage[i], 16).toString(2).padStart(4, "0");
+  }
+
+  // Polynôme générateur utilisé pour le calcul du CRC (CRC-16-CCITT)
+  const polynomial = "11000000000000101";
+
+  // Initialisation du registre CRC à 0xFFFF
+  let crcRegister = parseInt("FFFF", 16);
+
+  // Calcul du CRC
+  for (let i = 0; i < binaryMessage.length - 16; i++) {
+    const xorBit = (crcRegister & parseInt("8000", 16)) !== 0;
+    crcRegister <<= 1;
+    if (binaryMessage[i] === "1") {
+      crcRegister |= 1;
+    }
+    if (xorBit) {
+      crcRegister ^= parseInt(polynomial, 2);
+    }
+  }
+
+  // Vérification du CRC
+  const receivedCRC = binaryMessage.substr(binaryMessage.length - 16);
+  return crcRegister.toString(2).padStart(16, "0") === receivedCRC;
+}
+
+
+
+
 
 
 
---
+++ Readme.txt
@@ -0,0 +1,10 @@
+Protocole graphique Théo NICOLAS G3
+
+Pour lancer correctement le programme :
+
+- ouvrir le fichier "Hexa_circle.html" dans n'importe quel navigateur web
+- tapez votre message dans la zone de texte
+- Cliquer sur le bouton "Encoder" pour lancer l'encodage du message OU sur le bouton "Encodage Simple" pour lancer l'encodage sans détection d'Erreur
+- Pour relancer une deuxième fois recharger la page => F5 ou cmd + R sur mac (si cela ne marche pas => CTRL + F5)
+
+Conseil : Pour mieux comprendre lancer avec l'encodage simple avec un message court comme "a" qui correspond à 61 en hexa
